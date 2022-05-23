c54e29258835436b7aa4e21f0bb985a86d0d99bd
PavloKPO <pavlokolykhanov@gmail.com>
2022-05-23 18:03:26 +0300

    3.2 Scripts, life cycle, interactions

--- Assets/Game/Scripts/GameBoard.cs
+++ Assets/Game/Scripts/GameBoard.cs
@@ -6,25 +6,50 @@ public class GameBoard : MonoBehaviour
     [SerializeField] private SpriteRenderer _spriteRenderer;
     [SerializeField] private GameObject _tileFish;
     [SerializeField] private List<Sprite> _tileFishSprite = new List<Sprite>();
-
+    [SerializeField] private GameObject _tile;
+             
     [SerializeField] private Vector2Int _size;
     [SerializeField] private Vector2 _distance;
     [SerializeField] private Vector2 _startPosition;
 
+
+
+    [ContextMenu("Create Tiles")]
+    private void CreateTiles()
+    {
+        Vector2 position = _startPosition;
+
+        for (int y = 0; y < _size.y; y++)
+        {
+
+            for (int x = 0; x < _size.x; x++)
+            {
+                Instantiate(_tile, position, Quaternion.identity);
+                position.x += _distance.x;
+            }
+
+            position.x =  _startPosition.x;
+            position.y -= _distance.y;
+
+
+        }
+    }
     private void CreateGameBoard()
     {
-        Vector2 _position = new Vector2(_startPosition.x, _startPosition.y);
+        Vector2 position = _startPosition;
         
         for (int x = 0; x < _size.x; x++)
         {
-            Instantiate(_tileFish, _position, Quaternion.identity);
-            _position.x += _distance.x ;
+            Instantiate(_tileFish, position, Quaternion.identity);
+            position.x += _distance.x ;
         }
 
     }
 
+
     void Start()
     {
+        CreateTiles();
         CreateGameBoard();
     }
 
